f7549e2b1e58d83479ea40b1a46e15e728940d41
alive1216 <alice14i16@gmail.com>
2023-12-05 18:44:33 -0500

    Ejercicio 1.2

--- part1/src/index.js
+++ part1/src/index.js
@@ -21,16 +21,17 @@ const App = () => {
   
   const name="Juan"
   const age=25
-  return [/*return enviado como array*/
-    
+  return (
+    <>
       <h1>Greetings</h1>,
       <Hello name="Alison" age={12+3}/>,
       <Hello name={name} age={age}/>,
       <Hello name={name} age="15"/>,
       <footer/> /*Usada de una manera, como resultado se vera el componente*/,
-      <footer/> /*Usada de otra manera, como resultado NO se vera el componente*/
+      {Footer()} /*Usada de otra manera, como resultado se vera el codigo html*/,
+    </>
     
-  ]
+  )
 }
 
 ReactDOM.render(<App/>, document.getElementById('root')) 
